5110319093b98e2a877aa344cebab752d9451b55
rahulydv <ry040799@gmail.com>
2023-11-21 17:37:13 +0530

    dijkstra algo

--- Graph-3/src/Djkstra.java
+++ Graph-3/src/Djkstra.java
@@ -5,21 +5,20 @@ public class Djkstra {
     public ArrayList<Integer> solve(int A, ArrayList<ArrayList<Integer>> B, int C) {
         Graph1 g = new Graph1(A, B);
         ArrayList<Integer> res = g.djx(C);
-        return null;
+        for(int i=0; i<res.size(); i++){
+            if(res.get(i)==Integer.MAX_VALUE)   res.set(i, -1);
+        }
+        return res;
     }
 
     public static class Graph1 {
         int nodes;
         Map<Integer, ArrayList<Pair>> edges = new HashMap<>();
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
         public int[] visited;
 
         Graph1(int A, ArrayList<ArrayList<Integer>> B) {
 
             nodes = A;
-            for(int i=0; i<nodes; i++){
-                pq.add(new Pair(0, Integer.MAX_VALUE));
-            }
             visited = new int[A];
             for (ArrayList<Integer> integers : B) {
 
@@ -38,7 +37,7 @@ public class Djkstra {
 
         public ArrayList<Integer> djx(int startNode){
 
-            PriorityQueue<Pair> p = new PriorityQueue<>();
+            PriorityQueue<Pair> p = new PriorityQueue<>((a, b) -> a.w - b.w );
             ArrayList<Integer> dist = new ArrayList<>(Collections.nCopies(nodes, Integer.MAX_VALUE));
 
             p.add(new Pair(startNode, 0));
@@ -49,15 +48,44 @@ public class Djkstra {
                 visited[currPair.n] = 1;
 
                 ArrayList<Pair> currArr = edges.get(currPair.n);
-                if (currArr == null) return null;
+                if (currArr == null) return dist;
                 for(Pair pair: currArr){
-                    if(dist.get(pair.n)> (dist.get(currPair.n) + pair.w)){
+                    if(dist.get(pair.n)> (dist.get(currPair.n) + pair.w) && visited[pair.n]==0){
+
                         dist.set(pair.n, dist.get(currPair.n) + pair.w);
+                        p.add(new Pair(pair.n, dist.get(pair.n)));
                     }
-                    p.add(new Pair(pair.n, dist.get(pair.n)));
+
                 }
             }
-            return null;
+            return dist;
+        }
+
+        public ArrayList<Integer> djx2(int startNode) {
+            ArrayList<Integer> dist = new ArrayList<>(Collections.nCopies(nodes, Integer.MAX_VALUE));
+            dist.set(startNode, 0);
+
+            for(int i=0; i<nodes; i++){
+                int smallest  = Integer.MAX_VALUE;
+                int currNode = -1;
+                for(int j=0; j<nodes; j++){
+                    if(visited[j]==0 && dist.get(j)<smallest){
+                        smallest = dist.get(j);
+                        currNode = j;
+                    }
+                }
+
+                visited[currNode] = 1;
+
+                ArrayList<Pair> currArr = edges.get(currNode);
+                if (currArr == null) return null;
+                for(Pair pair: currArr) {
+                    if (dist.get(pair.n) > (dist.get(currNode) + pair.w)) {
+                        dist.set(pair.n, dist.get(currNode) + pair.w);
+                    }
+                }
+            }
+            return dist;
         }
     }
 
--- Graph-3/src/Main.java
+++ Graph-3/src/Main.java
@@ -5,29 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
-//        B.add(0, new ArrayList<>(List.of(6, 3)));
-//        B.add(1, new ArrayList<>(List.of(6, 1)));
-//        B.add(2, new ArrayList<>(List.of(5, 1)));
-//        B.add(3, new ArrayList<>(List.of(5, 2)));
-//        B.add(4, new ArrayList<>(List.of(3, 4)));
-//        B.add(5, new ArrayList<>(List.of(4, 2)));
-//        B.add(6, new ArrayList<>(List.of(2, 5)));
-//        B.add(7, new ArrayList<>(List.of(3, 8)));
-//        B.add(8, new ArrayList<>(List.of(4, 8)));
+        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
+        B.add(0, new ArrayList<>(List.of(0, 4, 9)));
+        B.add(1, new ArrayList<>(List.of(3, 4, 6)));
+        B.add(2, new ArrayList<>(List.of(1, 2, 1)));
+        B.add(3, new ArrayList<>(List.of(2, 5, 1)));
+        B.add(4, new ArrayList<>(List.of(2, 4, 5)));
+        B.add(5, new ArrayList<>(List.of(0, 3, 7)));
+        B.add(6, new ArrayList<>(List.of(0, 1, 1)));
+        B.add(7, new ArrayList<>(List.of(4, 5, 7)));
+        B.add(8, new ArrayList<>(List.of(0, 5, 1)));
 //        B.add(9, new ArrayList<>(List.of(14, 11)));
 //        B.add(10, new ArrayList<>(List.of(12, 9)));
 //        B.add(11, new ArrayList<>(List.of(2, 1)));
 //        B.add(12, new ArrayList<>(List.of(9, 2)));
 
 
-        int A = 70;
-        ArrayList<Integer> B = new ArrayList<>(List.of(67,8,48,42,35,25,37,69,31,36,7,33,2,47,42,52,31,70,29,38,36,60,15,37,33,27,4,32,43,55,49,35,21,28,62,17,2,61,54,22,9,56,12,3,60,52,21,15,54,63,33,64,38,16,59,69,49,52,10,10,6,56,43,32,41,66,6));
-        ArrayList<Integer> C = new ArrayList<>(List.of(51,43,55,27,34,8,14,5,70,64,65,57,45,19,53,50,44,51,19,41,14,68,12,58,50,66,7,47,40,62,29,5,22,39,23,34,25,4,40,26,26,45,18,28,61,59,17,46,39,46,68,24,63,59,67,53,9,11,3,44,24,37,13,1,65,18,48));
+        int A = 6;
+        int C = 4;
+//        ArrayList<Integer> B = new ArrayList<>(List.of(67,8,48,42,35,25,37,69,31,36,7,33,2,47,42,52,31,70,29,38,36,60,15,37,33,27,4,32,43,55,49,35,21,28,62,17,2,61,54,22,9,56,12,3,60,52,21,15,54,63,33,64,38,16,59,69,49,52,10,10,6,56,43,32,41,66,6));
+//        ArrayList<Integer> C = new ArrayList<>(List.of(51,43,55,27,34,8,14,5,70,64,65,57,45,19,53,50,44,51,19,41,14,68,12,58,50,66,7,47,40,62,29,5,22,39,23,34,25,4,40,26,26,45,18,28,61,59,17,46,39,46,68,24,63,59,67,53,9,11,3,44,24,37,13,1,65,18,48));
 //        TopologicalSort ts = new TopologicalSort();
-//        ArrayList<Integer> resp = ts.solve(A, B);
-        PossibilityOfFinishing pf = new PossibilityOfFinishing();
-        int resp = pf.solve(A, B, C);
+        Djkstra d = new Djkstra();
+        ArrayList<Integer> resp = d.solve(A, B, C);
+//        PossibilityOfFinishing pf = new PossibilityOfFinishing();
+
+//        int resp = d.solve(A, B, C);
         System.out.println("Final resp:: "+resp);
     }
 }
