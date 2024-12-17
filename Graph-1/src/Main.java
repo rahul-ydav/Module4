075155ee65df838ac4913e03b345bee8d5b12de0
rahulydv <ry040799@gmail.com>
2024-12-17 23:26:53 +0530

    no of island, solution

--- Graph-1/src/Main.java
+++ Graph-1/src/Main.java
@@ -6,21 +6,18 @@ public class Main {
 
         System.out.println("Hello world!");
 
-        int A = 4;
-        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
+//        int A = 4;
+//        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
+//
 //        B.add(0, new ArrayList<>(List.of(1, 2)));
-//        B.add(1, new ArrayList<>(List.of(4, 1)));
-//        B.add(2, new ArrayList<>(List.of(2, 4)));
-//        B.add(3, new ArrayList<>(List.of(3, 4)));
-//        B.add(4, new ArrayList<>(List.of(5, 2)));
-//        B.add(5, new ArrayList<>(List.of(1, 3)));
+//        B.add(1, new ArrayList<>(List.of(2, 3)));
+//        B.add(2, new ArrayList<>(List.of(4, 3)));
+//
+//        DirectedPath dPath = new DirectedPath();
+//        int resp = dPath.solve(A, B);
+//        System.out.println("Final resp:: "+resp);
 
-        B.add(0, new ArrayList<>(List.of(1, 2)));
-        B.add(1, new ArrayList<>(List.of(2, 3)));
-        B.add(2, new ArrayList<>(List.of(4, 3)));
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
 
-        DirectedPath dPath = new DirectedPath();
-        int resp = dPath.solve(A, B);
-        System.out.println("Final resp:: "+resp);
     }
 }
--- Graph-1/src/NoOfIslands.java
+++ Graph-1/src/NoOfIslands.java
@@ -1,7 +1,57 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class NoOfIslands {
+
+    boolean isValid(int x, int y, int a, int b, ArrayList<ArrayList<Integer>> A){
+        if(x==0 && y==0)    return false;
+        if(x+a<0 || x+a>=A.size())  return false;
+        if(y+b<0 || y+b>=A.get(0).size())   return false;
+        return A.get(x+a).get(y+b) == 1;
+    }
     public int solve(ArrayList<ArrayList<Integer>> A) {
-        return 0;
+        int result = 0;
+        int[] dir = {-1, 0, 1};
+
+        for(int i=0; i<A.size(); i++){
+            for(int j=0; j<A.get(0).size(); j++){
+                if(A.get(i).get(j)==1){
+                    result++;
+                    Queue<Pair> q = new LinkedList<>();
+                    q.add(new Pair(i, j));
+
+                    while(!q.isEmpty()){
+                        Pair currNode = q.poll();
+                        for(int x: dir){
+                            for(int y: dir){
+                                if(isValid(x, y, currNode.getA(), currNode.getB(), A)) {
+                                    q.add(new Pair(currNode.getA()+x, currNode.getB()+y));
+                                    A.get(currNode.getA()+x).set(currNode.getB()+y, -1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public class Pair{
+        private int a;
+        private int b;
+        Pair(int a, int b){
+            this.a = a;
+            this.b = b;
+        }
+        int getA(){
+            return this.a;
+        }
+        int getB(){
+            return this.b;
+        }
     }
 }
